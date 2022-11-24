f228090e412df7e74f95b80d10176e67ee9a8bc1
Tomoyuki Sakurai <y@trombik.org>
2022-11-24 13:34:16 +0700

    feat(switch): introduce smart detection switches

--- custom_components/yi_hack/switch.py
+++ custom_components/yi_hack/switch.py
@@ -7,25 +7,52 @@ from homeassistant.components.switch import SwitchEntity
 from homeassistant.const import (CONF_HOST, CONF_MAC, CONF_NAME,
                                  CONF_PASSWORD, CONF_PORT, CONF_USERNAME)
 from homeassistant.helpers.device_registry import CONNECTION_NETWORK_MAC
+from homeassistant.exceptions import HomeAssistantError
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
 
 from .common import (get_privacy, set_power_off_in_progress,
                      set_power_on_in_progress, set_privacy)
-from .const import DEFAULT_BRAND, DOMAIN
+from .const import (DEFAULT_BRAND, DOMAIN, CONF_HACK_NAME,
+                    DEFAULT_BRAND, MSTAR, ALLWINNER, ALLWINNERV2,
+                    V5, SONOFF)
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
+async def async_setup_entry(hass: HomeAssistant, config: ConfigEntry, async_add_entities):
     """Set up the Yi Camera media player from a config entry."""
-    async_add_entities([YiHackSwitch(hass, config_entry)], True)
+    entities = []
+
+    if (config.data[CONF_HACK_NAME] == DEFAULT_BRAND) or (config.data[CONF_HACK_NAME] == MSTAR):
+        entities = [
+            YiHackSwitch(config, "motion_detection"),
+            YiHackSwitch(config, "baby_crying"),
+        ]
+    elif (config.data[CONF_HACK_NAME] == ALLWINNER) or (config.data[CONF_HACK_NAME] == V5):
+        entities = [
+            YiHackSwitch(config, "motion_detection"),
+            YiHackSwitch(config, "baby_crying"),
+            YiHackSwitch(config, "sound_detection"),
+        ]
+    elif (config.data[CONF_HACK_NAME] == ALLWINNERV2):
+        entities = [
+            YiHackSwitch(config, "motion_detection"),
+            YiHackSwitch(config, "human_detection"),
+            YiHackSwitch(config, "baby_crying"),
+            YiHackSwitch(config, "sound_detection"),
+        ]
+    elif config.data[CONF_HACK_NAME] == SONOFF:
+        entities = [
+            YiHackSwitch(config, "motion_detection"),
+        ]
+    async_add_entities(entities)
 
 class YiHackSwitch(SwitchEntity):
-    """Representation of a Yi Camera Switch."""
+    """Representation of Yi Camera Switches."""
 
-    def __init__(self, hass, config):
-        """Initialize the device."""
+    def __init__(self, config, switch_type):
+        """Initialize the switch."""
         self._device_name = config.data[CONF_NAME]
-        self._name = self._device_name + "_privacy"
-        self._unique_id = self._device_name + "_swpr"
         self._mac = config.data[CONF_MAC]
         self._host = config.data[CONF_HOST]
         self._port = config.data[CONF_PORT]
@@ -33,49 +60,87 @@ class YiHackSwitch(SwitchEntity):
         self._password = config.data[CONF_PASSWORD]
         self._state = False
 
+        if switch_type == "motion_detection":
+            self._unique_id = self._device_name + "_swmd"
+        elif switch_type == "human_detection":
+            self._unique_id = self._device_name + "_swhd"
+        elif switch_type == "baby_crying":
+            self._unique_id = self._device_name + "_swbc"
+        elif switch_type == "sound_detection":
+            self._unique_id = self._device_name + "_swsd"
+        elif switch_type == "privacy":
+            self._unique_id = self._device_name + "_swpr"
+        else:
+            raise HomeAssistantError(
+                f"Unknown switch type: {switch_type}"
+            )
+        self._name = self._device_name + "_" + switch_type
+        self._config = dict([
+            (CONF_HOST, self._host),
+            (CONF_PORT, self._port),
+            (CONF_USERNAME, self._user),
+            (CONF_PASSWORD, self._password),
+        ])
+
+
     def update(self):
         """Return the state of the switch."""
-        conf = dict([
-            (CONF_HOST, self._host),
-            (CONF_PORT, self._port),
-            (CONF_USERNAME, self._user),
-            (CONF_PASSWORD, self._password),
-        ])
-        self._state = get_privacy(self.hass, self._device_name, conf)
+        if self._unique_id.endswith("_swpr"):
+            self._state = get_privacy(self.hass, self._device_name, self._config)
+        elif self._unique_id.endswith("_swmd"):
+            pass
+        elif self._unique_id.endswith("_swhd"):
+            pass
+        elif self._unique_id.endswith("_swbc"):
+            pass
+        elif self._unique_id.endswith("_swsd"):
+            pass
+        else:
+            _LOGGER.error("Unknown _unique_id: `%s`", self._unique_id)
 
     def turn_off(self):
-        """Turn off privacy (set camera on)."""
-        conf = dict([
-            (CONF_HOST, self._host),
-            (CONF_PORT, self._port),
-            (CONF_USERNAME, self._user),
-            (CONF_PASSWORD, self._password),
-        ])
-        if get_privacy(self.hass, self._device_name):
-            _LOGGER.debug("Turn off privacy, camera %s", self._name)
-            # Turn off the privacy switch:
-            # power on the cam and set privacy false
-            set_power_on_in_progress(self.hass, self._device_name)
-            set_privacy(self.hass, self._device_name, False, conf)
-            self._state = False
-            self.schedule_update_ha_state(force_refresh=True)
+        """Turn off the switch"""
+        if self._unique_id.endswith("_swpr"):
+            if get_privacy(self.hass, self._device_name):
+                _LOGGER.debug("Turn off privacy, camera %s", self._name)
+                # Turn off the privacy switch:
+                # power on the cam and set privacy false
+                set_power_on_in_progress(self.hass, self._device_name)
+                set_privacy(self.hass, self._device_name, False, self._config)
+                self._state = False
+                self.schedule_update_ha_state(force_refresh=True)
+        elif self._unique_id.endswith("_swmd"):
+            pass
+        elif self._unique_id.endswith("_swhd"):
+            pass
+        elif self._unique_id.endswith("_swbc"):
+            pass
+        elif self._unique_id.endswith("_swsd"):
+            pass
+        else:
+            _LOGGER.error("Unknown _unique_id: `%s`", self._unique_id)
 
     def turn_on(self):
-        """Turn on privacy (set camera off)."""
-        conf = dict([
-            (CONF_HOST, self._host),
-            (CONF_PORT, self._port),
-            (CONF_USERNAME, self._user),
-            (CONF_PASSWORD, self._password),
-        ])
-        if not get_privacy(self.hass, self._device_name):
-            _LOGGER.debug("Turn on privacy, camera %s", self._name)
-            # Turn on the privacy switch:
-            # power off the cam and set privacy true
-            set_power_off_in_progress(self.hass, self._device_name)
-            set_privacy(self.hass, self._device_name, True, conf)
-            self._state = True
-            self.schedule_update_ha_state(force_refresh=True)
+        """Turn on the switch."""
+        if self._unique_id.endswith("_swpr"):
+            if not get_privacy(self.hass, self._device_name):
+                _LOGGER.debug("Turn on privacy, camera %s", self._name)
+                # Turn on the privacy switch:
+                # power off the cam and set privacy true
+                set_power_off_in_progress(self.hass, self._device_name)
+                set_privacy(self.hass, self._device_name, True, self._config)
+                self._state = True
+                self.schedule_update_ha_state(force_refresh=True)
+        elif self._unique_id.endswith("_swmd"):
+            pass
+        elif self._unique_id.endswith("_swhd"):
+            pass
+        elif self._unique_id.endswith("_swbc"):
+            pass
+        elif self._unique_id.endswith("_swsd"):
+            pass
+        else:
+            _LOGGER.error("Unknown _unique_id: `%s`", self._unique_id)
 
     @property
     def is_on(self):
