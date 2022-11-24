5f1f0afb4bf33ed0acbfd78112a833a4ef2f956b
Tomoyuki Sakurai <y@trombik.org>
2022-11-24 12:23:58 +0700

    refactor(common): introduce call_api(), replace API calls with it

--- custom_components/yi_hack/common.py
+++ custom_components/yi_hack/common.py
@@ -28,86 +28,29 @@ _LOGGER = logging.getLogger(__name__)
 
 def get_status(config):
     """Get system status from camera."""
-    host = config[CONF_HOST]
-    port = config[CONF_PORT]
-    user = config[CONF_USERNAME]
-    password = config[CONF_PASSWORD]
     error = False
-
-    auth = None
-    if user or password:
-        auth = HTTPBasicAuth(user, password)
-
-    response = None
     try:
-        response = requests.get("http://" + host + ":" + str(port) + "/cgi-bin/status.json", timeout=HTTP_TIMEOUT, auth=auth)
-        if response.status_code >= 300:
-            _LOGGER.error("Failed to get status from device %s", host)
-            error = True
-    except requests.exceptions.RequestException as e:
-        _LOGGER.error("Failed to get status from device %s: error %s", host, e)
+        response = call_api(config, "status.json", None)
+    except:
         error = True
-
     if error:
-        response = None
         return None
-
     return response.json()
 
 def get_system_conf(config):
     """Get system configuration from camera."""
-    host = config[CONF_HOST]
-    port = config[CONF_PORT]
-    user = config[CONF_USERNAME]
-    password = config[CONF_PASSWORD]
-    error = False
-
-    auth = None
-    if user or password:
-        auth = HTTPBasicAuth(user, password)
-
     response = None
-    try:
-        response = requests.get("http://" + host + ":" + str(port) + "/cgi-bin/get_configs.sh?conf=system", timeout=HTTP_TIMEOUT, auth=auth)
-        if response.status_code >= 300:
-            _LOGGER.error("Failed to get system configuration from device %s", host)
-            error = True
-    except requests.exceptions.RequestException as e:
-        _LOGGER.error("Failed to get system configuration from device %s: error %s", host, e)
-        error = True
-
-    if error:
-        response = None
+    response = call_api(config, "get_configs.sh", "conf=system")
+    if response is None:
         return None
-
     return response.json()
 
 def get_mqtt_conf(config):
     """Get mqtt configuration from camera."""
-    host = config[CONF_HOST]
-    port = config[CONF_PORT]
-    user = config[CONF_USERNAME]
-    password = config[CONF_PASSWORD]
-    error = False
-
-    auth = None
-    if user or password:
-        auth = HTTPBasicAuth(user, password)
-
     response = None
-    try:
-        response = requests.get("http://" + host + ":" + str(port) + "/cgi-bin/get_configs.sh?conf=mqtt", timeout=HTTP_TIMEOUT, auth=auth)
-        if response.status_code >= 300:
-            _LOGGER.error("Failed to get mqtt configuration from device %s", host)
-            error = True
-    except requests.exceptions.RequestException as e:
-        _LOGGER.error("Failed to get mqtt configuration from device %s: error %s", host, e)
-        error = True
-
-    if error:
-        response = None
+    response = call_api(config, "get_configs.sh", "conf=mqtt")
+    if response is None:
         return None
-
     return response.json()
 
 def get_privacy(hass, device_name, config=None):
@@ -121,36 +64,14 @@ def get_privacy(hass, device_name, config=None):
 
     if config is None:
         return hass.data[DOMAIN][device_name][PRIVACY]
-
-    host = config[CONF_HOST]
-    port = config[CONF_PORT]
-    user = config[CONF_USERNAME]
-    password = config[CONF_PASSWORD]
     error = False
-
-    auth = None
-    if user or password:
-        auth = HTTPBasicAuth(user, password)
-
-    response = None
-    try:
-        response = requests.get("http://" + host + ":" + str(port) + "/cgi-bin/privacy.sh?value=status", timeout=HTTP_TIMEOUT, auth=auth)
-        if response.status_code >= 300:
-            _LOGGER.error("Failed to get status of device %s", host)
-            error = True
-    except requests.exceptions.RequestException as e:
-        _LOGGER.error("Error getting status of device %s: error %s", host, e)
-        error = True
-
-    if error:
-        return None
-
-    if response is not None:
+    response = call_api(config, "privacy.sh", "value=status")
+    if response is not None and response:
         try:
             privacy_dict: dict = response.json()
             privacy: str = privacy_dict.get("status")
         except KeyError:
-            _LOGGER.error("Failed to get status of device %s: error unknown", host)
+            _LOGGER.error("Response does not have key `status` on device %s", host)
             error = True
     else:
         _LOGGER.error("Failed to get status on device %s: error unknown", host)
@@ -175,51 +96,29 @@ def set_privacy(hass, device_name, newstatus, config=None):
     if config is None:
         hass.data[DOMAIN][device_name][PRIVACY] = newstatus
         return
-
-    host = config[CONF_HOST]
-    port = config[CONF_PORT]
-    user = config[CONF_USERNAME]
-    password = config[CONF_PASSWORD]
     error = False
     if newstatus:
         newstatus_string = "on"
     else:
         newstatus_string = "off"
-
-    auth = None
-    if user or password:
-        auth = HTTPBasicAuth(user, password)
-
-    response = None
-    try:
-        response = requests.get("http://" + host + ":" + str(port) + "/cgi-bin/privacy.sh?value=" + newstatus_string, timeout=HTTP_TIMEOUT, auth=auth)
-        if response.status_code >= 300:
-            _LOGGER.error("Failed to switch on device %s", host)
-            error = True
-    except requests.exceptions.RequestException as e:
-        _LOGGER.error("Failed to switch on device %s: error %s", host, e)
-        error = True
-
-    if error:
-        return None
-
-    if response is not None:
+    response = call_api(config, "privacy.sh", "value=" + newstatus_string)
+    if response is not None and response:
         try:
             if response.json()["status"] != "on" and response.json()["status"] != "off":
-                _LOGGER.error("Failed to switch on device %s", host)
+                _LOGGER.error("Returned status is neither on nor off on device %s", host)
                 error = True
         except KeyError:
-            _LOGGER.error("Failed to switch on device %s: error unknown", host)
+            _LOGGER.error("Response does not have key `status` on device %s", host)
+            error = True
+        except requests.exceptions.JSONDecodeError:
+            _LOGGER.error("Invalid JSON returned: %s on device %s", response.text, host)
             error = True
     else:
         _LOGGER.error("Failed to switch on device %s: error unknown", host)
         error = True
-
     if error:
         return False
-
     hass.data[DOMAIN][device_name][PRIVACY] = newstatus
-
     return True
 
 def set_power_off_in_progress(hass, device_name):
@@ -248,3 +147,35 @@ def get_device_conf(hass, device_name, param=None):
     if param is None:
         return hass.data[DOMAIN][device_name]
     return hass.data[DOMAIN][device_name][param]
+
+def call_api(config, api_name, query_string):
+    """ Send HTTP GET request to API under /cgi-bin with a query string"""
+    host = config[CONF_HOST]
+    port = config[CONF_PORT]
+    user = config[CONF_USERNAME]
+    password = config[CONF_PASSWORD]
+    error = False
+    auth = None
+    if user or password:
+        auth = HTTPBasicAuth(user, password)
+    response = None
+    if query_string is None:
+        query_string = ""
+    else:
+        query_string = "?" + query_string
+    try:
+        full_url = "http://" + host + ":" + str(port) + "/cgi-bin/" + api_name + query_string
+        _LOGGER.debug("call_api: full_url: `%s`", full_url)
+        response = requests.get(full_url, timeout=HTTP_TIMEOUT, auth=auth)
+        if response.status_code >= 300:
+            _LOGGER.error("unexpected HTTP status code by %s API on device %s (status: %d)", api_name, host, response.status_code)
+            error = True
+    except (requests.exceptions.Timeout, requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout, requests.exceptions.Timeout) as e:
+        _LOGGER.error("%s API timed out on %s (%d sec), %s", api_name, host, HTTP_TIMEOUT, e)
+        error = True
+    except:
+        _LOGGER.error("%s API failed on device %s: error %s", api_name, host, e)
+        error = True
+    if error:
+        return None
+    return response
